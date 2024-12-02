d87cfbdc927bdb573ba7f676db00b9e589c7ce53
Jaldhar H. Vyas <jaldhar@braincells.com>
2024-12-01 20:39:58 -0500

    Kotlin 2024 Day 1: Historian Hysteria.

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,18 +1,57 @@
+// Advent of Code
+// Solution by Jaldhar H. Vyas <jaldhar@braincells.com>
+// Copyright (C) 2024, Consolidated Braincells Inc.  All rights reserved.
+// "Do What Thou Wilt" shall be the whole of the license.
+
 fun main() {
+    val re = Regex("""\d+""")
+
     fun part1(input: List<String>): Int {
-        return input.size
+        var left = mutableListOf<Int>()
+        var right = mutableListOf<Int>()
+
+        for (line in input) {
+            val matches = re.findAll(line).toList()
+            left.add(matches[0].value.toInt())
+            right.add(matches[1].value.toInt())
+        }
+
+        left.sort()
+        right.sort()
+
+        var distances = mutableListOf<Int>()
+
+        for (p in (left zip right)) {
+            distances.add(Math.abs(p.first - p.second))
+        }
+
+        return distances.sum()
     }
 
     fun part2(input: List<String>): Int {
-        return input.size
+        var left = mutableListOf<Int>()
+        var right = mutableListOf<Int>()
+
+        for (line in input) {
+            val matches = re.findAll(line).toList()
+            left.add(matches[0].value.toInt())
+            right.add(matches[1].value.toInt())
+         }
+
+         var similarity: Int = 0
+         for (n in left) {
+             similarity += right.filter({it == n}).sum()
+         }
+
+         return similarity
     }
 
     // Test if implementation meets criteria from the description, like:
-    check(part1(listOf("test_input")) == 1)
+    // check(part1(listOf("test_input")) == 1)
 
     // Or read a large test input from the `src/Day01_test.txt` file:
-    val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
+    // val testInput = readInput("Day01_test")
+    // check(part1(testInput) == 1)
 
     // Read the input from the `src/Day01.txt` file.
     val input = readInput("Day01")
